1aa9ed7ca8045d24c0cc3e0de824d32cdc168b78
Todd Wolfson <todd@twolfson.com>
2014-02-09 17:51:34 -0800

    Completed task set for tests

---
+++ test/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var spritesmith = require('../');
+
+// Define our tests
+var images = [
+  'test-files/1.png',
+  'test-files/2.jpg',
+  'test-files/3.png'
+];
+gulp.task('sprite-default', function () {
+  var spriteData = gulp.src(images).pipe(spritesmith());
+  spriteData.img.pipe(gulp.dest('expected-files/default/sprite.png'));
+  spriteData.css.pipe(gulp.dest('expected-files/default/sprite.css'));
+});
+gulp.task('sprite-formats', function () {
+  var spriteData = gulp.src(images).pipe(spritesmith({
+    imgFormat: 'jpg',
+    cssFormat: 'styl'
+  }));
+  spriteData.img.pipe(gulp.dest('expected-files/default/sprite.jpg'));
+  spriteData.css.pipe(gulp.dest('expected-files/default/sprite.styl'));
+});
+gulp.task('sprite-options', function () {
+  var spriteData = gulp.src(images).pipe(spritesmith({
+    algorithm: 'alt-diagonal',
+    engine: 'gm'
+  }));
+  spriteData.img.pipe(gulp.dest('expected-files/default/sprite.png'));
+  spriteData.css.pipe(gulp.dest('expected-files/default/sprite.css'));
+});
+
+// Define a group task for tests
+gulp.task('sprite', ['sprite-default', 'sprite-formats']);
