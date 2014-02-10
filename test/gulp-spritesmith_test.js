34f647bd88b1b91f4c6cdd63d775c75c634867cb
Todd Wolfson <todd@twolfson.com>
2014-02-09 17:58:01 -0800

    Filled out test suite outline

--- test/gulp-spritesmith_test.js
+++ test/gulp-spritesmith_test.js
@@ -1,11 +1,33 @@
 var gulp_spritesmith = require('../');
 
 describe('gulp-spritesmith', function () {
-  before(function () {
+  describe('running a task without any options', function () {
+    it.skip('generates a top-down png', function () {
 
+    });
+
+    it.skip('generates a css file', function () {
+
+    });
   });
 
-  it('', function () {
+  describe('running a task with output formats', function () {
+    it.skip('generates a top-down jpg', function () {
 
+    });
+
+    it.skip('generates a Stylus file', function () {
+
+    });
+  });
+
+  describe('running a task with engine and algorithm options', function () {
+    it.skip('generates an alt-diagonal png via the gm engine', function () {
+
+    });
+
+    it.skip('generates a CSS file with alt-diagonal coordinates', function () {
+
+    });
   });
 });
