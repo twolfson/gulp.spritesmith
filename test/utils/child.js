4c021b24c51854eb3dbd1fbe97caede0221c0c06
Todd Wolfson <todd@twolfson.com>
2014-12-14 23:26:27 -0600

    Added missing code comments

--- test/utils/child.js
+++ test/utils/child.js
@@ -1,7 +1,9 @@
+// Load in dependencies
 var exec = require('child_process').exec;
 
+// Define our execution helper
 exports.run = function (cmd) {
-  before(function (done) {
+  before(function runFn (done) {
     exec(cmd, function (err, stdout, stderr) {
       if (!err && stderr) {
         err = new Error(stderr);
